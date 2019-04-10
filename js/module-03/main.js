3b5c7c09f002411b88424330b041e4a30c50be0f
Manuilenkoart <Manuilenkoart@gmail.com>
2019-04-10 22:14:34 +0300

    push

--- js/module-03/main.js
+++ js/module-03/main.js
@@ -2,32 +2,23 @@
 
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const isLoginValid = function(login) {
+const isLoginValid = login => {
   const letterArry = login.split("");
-  if (letterArry.length >= 4 && letterArry.length <= 16) {
-    return true;
-  } else {
-    return false;
-  }
+  return letterArry.length >= 4 && letterArry.length <= 16;
 };
 
-const isLoginUnique = function(allLogins, login) {
-  if (allLogins.includes(login)) {
-    return false; // логин есть
-  } else {
-    return true; // логина нет
-  }
+const isLoginUnique = (allLogins, login) => {
+  return allLogins.includes(login);
 };
 const addLogin = function(allLogins, login) {
   if (!isLoginValid(login)) {
     return "Ошибка! Логин должен быть от 4 до 16 символов";
   }
 
-  if (isLoginUnique(allLogins, login)) {
+  if (!isLoginUnique(allLogins, login)) {
     logins.push(login);
     return "Логин успешно добавлен!";
-  }
-  if (!isLoginUnique(allLogins, login)) {
+  } else {
     return "Такой логин уже используется!";
   }
 };
